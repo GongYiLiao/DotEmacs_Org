7b2128ef2cd1bfee04104467038ea2b12661654a
lkg <lkg@github.com>
2025-06-08 20:14:35 -0500

    more description of code clips

--- init_gen.el
+++ init_gen.el
@@ -2,7 +2,6 @@
 (require 'org)
 ;; Open the org-mode configuration
 (defvar src-readme-org (concat user-emacs-directory "README.org"))
-(defvar target-init (concat user-emacs-directory "init_2.el"))
 ;; Tangle the file
-(org-babel-tangle-file src-readme-org target-init)
+(org-babel-tangle-file src-readme-org)
 ;; init_gen.el ends here
